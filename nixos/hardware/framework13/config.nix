7df648a54518e2a3e3254bb89bff1f653d96f323
rhedgeco <rhedgeco@gmail.com>
2024-03-05 14:49:58 -0800

    fix root clearing issues

--- nixos/hardware/framework13/config.nix
+++ nixos/hardware/framework13/config.nix
@@ -63,24 +63,27 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot.initrd.postDeviceCommands = lib.mkAfter ''
-    if [[ -e /btrfs_root/root ]]; then
-        mkdir -p /btrfs_root/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_root/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_root/root "/btrfs_root/old_roots/$timestamp"
+    mkdir /btrfs_tmp
+    mount /dev/nvme0n1p3 /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
     fi
 
     delete_subvolume_recursively() {
         IFS=$'\n'
         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_root/$i"
+            delete_subvolume_recursively "/btrfs_tmp/$i"
         done
         btrfs subvolume delete "$1"
     }
 
-    for i in $(find /btrfs_root/old_roots/ -maxdepth 1 -mtime +30); do
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
         delete_subvolume_recursively "$i"
     done
 
-    btrfs subvolume create /btrfs_root/root
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
   '';
 }
