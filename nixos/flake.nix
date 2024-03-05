a667fa9e5e5a51e5238b8ed04986d2125ced1387
rhedgeco <rhedgeco@gmail.com>
2024-03-05 11:03:44 -0800

    add cachix and link home-manager to nixpkgs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,10 +1,24 @@
 {
   description = "hedge-config";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     impermanence.url = "github:nix-community/impermanence";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     nur.url = "github:nix-community/nur";
   };
