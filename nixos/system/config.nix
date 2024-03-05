c137bf69279d137b93ba0c2dbe8bf8c345a9840b
rhedgeco <rhedgeco@gmail.com>
2024-03-05 11:08:56 -0800

    enable automatic upgrades

--- nixos/system/config.nix
+++ nixos/system/config.nix
@@ -122,5 +122,20 @@
     };
   };
 
+  # automatic upgrades
+  # systemctl status nixos-upgrade.timer
+  # systemctl status nixos-upgrade.service
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
   system.stateVersion = "23.11"; # dont touch
 }
