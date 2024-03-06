e5572b1d858e686132e8d8bf162224289d18004c
rhedgeco <rhedgeco@gmail.com>
2024-03-05 17:04:41 -0800

    add rust-analyzer and reference system version from vscode

--- nixos/ryan/pkgs/rust.nix
+++ nixos/ryan/pkgs/rust.nix
@@ -5,6 +5,9 @@
 }: {
   nixpkgs.overlays = [inputs.rust-overlay.overlays.default];
   home.packages = with pkgs; [
-    rust-bin.stable.latest.default
+    (rust-bin.stable.latest.default.override
+      {
+        extensions = ["rust-src" "rust-analyzer"];
+      })
   ];
 }
