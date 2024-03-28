028c958cb774d515be9d1206f804ec7d446cedbc
rhedgeco <rhedgeco@gmail.com>
2024-03-27 17:06:53 -0700

    use crate2nix for dconf-sync

--- nixos/programs/dconf-sync/flake.nix
+++ nixos/programs/dconf-sync/flake.nix
@@ -1,17 +1,74 @@
 {
-  description = "sync dconf database with keyfiles";
+  description = "Rust-Nix";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crate2nix.url = "github:nix-community/crate2nix";
+
+    # Development
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./. { };
-      });
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    rust-overlay,
+    crate2nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        ./nix/rust-overlay/flake-module.nix
+        ./nix/devshell/flake-module.nix
+      ];
+
+      perSystem = {
+        system,
+        pkgs,
+        lib,
+        inputs',
+        ...
+      }: let
+        # If you dislike IFD, you can also generate it with `crate2nix generate`
+        # on each dependency change and import it here with `import ./Cargo.nix`.
+        cargoNix = inputs.crate2nix.tools.${system}.appliedCargoNix {
+          name = "rustnix";
+          src = ./.;
+        };
+      in rec {
+        checks = {
+          rustnix = cargoNix.rootCrate.build.override {
+            runTests = true;
+          };
+        };
+
+        packages = {
+          rustnix = cargoNix.rootCrate.build;
+          default = packages.rustnix;
+
+          inherit (pkgs) rust-toolchain;
+
+          rust-toolchain-versions = pkgs.writeScriptBin "rust-toolchain-versions" ''
+            ${pkgs.rust-toolchain}/bin/cargo --version
+            ${pkgs.rust-toolchain}/bin/rustc --version
+          '';
+        };
+      };
     };
 }
-
