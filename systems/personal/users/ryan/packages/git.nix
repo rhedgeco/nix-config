dd234f39a73f8c07abb48acdec18318e462de37c
rhedgeco <rhedgeco@gmail.com>
2024-08-21 11:29:16 -0700

    add gpg signing

--- systems/personal/users/ryan/packages/git.nix
+++ systems/personal/users/ryan/packages/git.nix
@@ -4,5 +4,11 @@
     lfs.enable = true;
     userName = "rhedgeco";
     userEmail = "rhedgeco@gmail.com";
+
+    extraConfig = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
   };
 }
