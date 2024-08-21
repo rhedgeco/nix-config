8f93f7e4c591189a81e3d723282d603541f8b267
rhedgeco <rhedgeco@gmail.com>
2024-08-21 11:33:52 -0700

    add allowed signers

--- systems/personal/users/ryan/packages/git.nix
+++ systems/personal/users/ryan/packages/git.nix
@@ -1,4 +1,6 @@
 {...}: {
+  home.file.".ssh/allowed_signers".text = "* ${builtins.readFile /home/ryan/.ssh/id_ed25519.pub}";
+
   programs.git = {
     enable = true;
     lfs.enable = true;
@@ -8,6 +10,7 @@
     extraConfig = {
       commit.gpgsign = true;
       gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       user.signingkey = "~/.ssh/id_ed25519.pub";
     };
   };
