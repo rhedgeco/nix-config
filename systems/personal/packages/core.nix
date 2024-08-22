7447709bf422c55d345e4ded51c35893014eedff
Ryan Hedgecock <rhedgeco@gmail.com>
2024-08-22 12:44:34 -0700

    switch gnome to cosmic

--- systems/personal/packages/core.nix
+++ systems/personal/packages/core.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
+  imports = [
+    # import home-manager and impermanence
+    inputs.home-manager.nixosModules.home-manager
+    inputs.impermanence.nixosModules.impermanence
+  ];
+
+  # set up core packages
   environment.systemPackages = with pkgs; [
     vim
     wget
---
+++ systems/personal/packages/cosmic.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+  imports = [
+    inputs.nixos-cosmic.nixosModules.default
+  ];
+
+  services.xserver = {
+    enable = true;
+    desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
+  };
+}
