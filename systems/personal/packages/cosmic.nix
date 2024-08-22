e5e6e1a54c182335c30f27c4cc99d3a855b6f5e4
Ryan Hedgecock <rhedgeco@gmail.com>
2024-08-22 12:49:16 -0700

    fix cosmic services

--- systems/personal/packages/cosmic.nix
+++ systems/personal/packages/cosmic.nix
@@ -3,12 +3,16 @@
   inputs,
   ...
 }: {
+  nix.settings = {
+    substituters = ["https://cosmic.cachix.org/"];
+    trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
+  };
+
   imports = [
     inputs.nixos-cosmic.nixosModules.default
   ];
 
-  services.xserver = {
-    enable = true;
+  services = {
     desktopManager.cosmic.enable = true;
     displayManager.cosmic-greeter.enable = true;
   };
