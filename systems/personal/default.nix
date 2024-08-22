66354c7ca31b6ddb70952a5d2eb4db959ffcbf7b
Ryan Hedgecock <rhedgeco@gmail.com>
2024-08-22 12:49:26 -0700

    remove pulseaudio

--- systems/personal/default.nix
+++ systems/personal/default.nix
@@ -8,7 +8,6 @@
   # system stuff
   sound.enable = true;
   xdg.mime.enable = true;
-  hardware.pulseaudio.enable = true;
   networking.networkmanager.enable = true;
   programs.fuse.userAllowOther = true;
   nixpkgs.config.allowUnfree = true;
