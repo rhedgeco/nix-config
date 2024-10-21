9f3b9a328c84e4994ddb118b836c2b204505b41f
Ryan Hedgecock <rhedgeco@gmail.com>
2024-10-21 09:38:25 -0700

    remove all unessential code items

--- systems/personal/packages/code.nix
+++ systems/personal/packages/code.nix
@@ -3,25 +3,19 @@
   pkgs,
   ...
 }: {
+  # only include base tools for editing nix
   environment.systemPackages = with pkgs; [
     nil
     alejandra
-    gcc
-    rustup
-    python3
     just
-    poetry
 
     (vscode-with-extensions.override {
       vscodeExtensions = with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
         jnoortheen.nix-ide
         kamadorueda.alejandra
-        rust-lang.rust-analyzer
-        tamasfe.even-better-toml
-        fill-labs.dependi
-        ms-python.python
-        ms-python.black-formatter
         kokakiwi.vscode-just
+        ms-vscode-remote.remote-containers
+        ms-azuretools.vscode-docker
       ];
     })
   ];
