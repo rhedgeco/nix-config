9458cca3547e72a901df612ae2ac3c379156e3b7
Ryan Hedgecock <rhedgeco@gmail.com>
2025-05-14 11:52:47 -0700

    remove bad extensions

--- systems/personal/modules/code.nix
+++ systems/personal/modules/code.nix
@@ -16,9 +16,6 @@
         kamadorueda.alejandra
         kokakiwi.vscode-just
         ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-containers
-        ms-vscode-remote.remote-ssh
-        ms-vscode.remote-server
         mkhl.direnv
       ];
     })
