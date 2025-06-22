58ef0a8a8fc0e634a83ca848536008d981019c51
Ryan Hedgecock <rhedgeco@gmail.com>
2025-06-21 20:23:04 -0700

    set up seperate filesystem config

--- hardware/framework13/intel-11gen/default.nix
+++ hardware/framework13/intel-11gen/default.nix
@@ -8,9 +8,12 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  # set hardware clock to use local time
+  # this is so that it matches for a windows dual boot
+  time.hardwareClockInLocalTime = true;
+
   # set time zone
   time.timeZone = "America/Los_Angeles";
-  time.hardwareClockInLocalTime = true; # so time matches windows boot
 
   # set boot modules
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
@@ -18,79 +21,10 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  # auto scrub btrfs filesystem
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-    fileSystems = ["/btrfs"];
-  };
-
-  # mount filesystem
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = ["subvol=root" "compress=zstd"];
-  };
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
-  };
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = ["subvol=home" "compress=zstd"];
-  };
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-label/NIXOS";
-    neededForBoot = true;
-    fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd"];
-  };
-  fileSystems."/btrfs" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = ["compress=zstd"];
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
-    fsType = "vfat";
-  };
-  swapDevices = [
-    {device = "/dev/disk/by-label/SWAP";}
-  ];
-
   # other system stuff
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  virtualisation.docker.storageDriver = "btrfs";
-
-  # create a root history on boot
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/disk/by-label/NIXOS /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/root_history
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/root_history/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
 
   # add special eGPU configuration
   specialisation = {
