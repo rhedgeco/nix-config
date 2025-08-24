c64120d00b7fb14735714a96021d35efbbc3348b
Ryan Hedgecock <rhedgeco@gmail.com>
2025-08-24 13:59:31 -0700

    add impermanence support to btrfs

--- modules/btrfs.nix
+++ modules/btrfs.nix
@@ -34,11 +34,50 @@
         options = ["subvol=nix" "compress=zstd" "noatime"];
       };
 
+      # mount the persist subvolume if impermanence is enabled
+      "/persist" = lib.mkIf config.custom.impermanence.enable {
+        device = config.custom.filesystem.btrfs.mainDevice;
+        fsType = "btrfs";
+        options = ["subvol=persist" "compress=zstd"];
+        neededForBoot = true;
+      };
+
       "/btrfs" = {
         device = config.custom.filesystem.btrfs.mainDevice;
         fsType = "btrfs";
         options = ["compress=zstd"];
       };
     };
+
+    # if impermanence is enabled, back up old root directories on boot
+    # this will create a fresh root each time and store backups for 30 days
+    boot.initrd.postDeviceCommands = lib.mkIf config.custom.impermanence.enable ''
+      mkdir /btrfs
+      mount -o compress=zstd ${config.custom.filesystem.btrfs.mainDevice} /btrfs
+      if [[ -e /btrfs/root ]]; then
+          echo "Found old root directory, creating backup..."
+          mkdir -p /btrfs/backup/root
+          timestamp=$(date --date="@$(stat -c %Y /btrfs/root)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /btrfs/root "/btrfs/backup/root/$timestamp"
+      fi
+
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs/$i"
+          done
+          echo "Found old backup at $1, deleting..."
+          btrfs subvolume delete "$1"
+      }
+
+      echo "Scanning root backups for roots older than 30 days..."
+      for i in $(find /btrfs/backup/root/* -maxdepth 0 -mtime +30); do
+          delete_subvolume_recursively "$i"
+      done
+
+      echo "Building fresh root directory..."
+      btrfs subvolume create /btrfs/root
+      umount /btrfs
+    '';
   };
 }
