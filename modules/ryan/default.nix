591ffaef4ed9bbae45c45e35ed737f1504b8c23b
Ryan Hedgecock <rhedgeco@gmail.com>
2025-10-01 18:13:48 -0700

    simplify the yoinkfile service

--- modules/ryan/default.nix
+++ modules/ryan/default.nix
@@ -21,27 +21,20 @@ in {
       extraGroups = ["wheel"];
     };
 
-    # create systemd service that populates all the dotfiles
+    # create systemd service that populates all the yoinkfiles
     systemd.services.yoink-push-ryan = {
-      enable = true;
       description = "Manages dotfiles for ryan";
+      wantedBy = ["multi-user.target"];
 
       serviceConfig = {
         User = "ryan";
         Group = "users";
-        WorkingDirectory = "/home/ryan";
-        ExecStart = "${yoink}/bin/yoink -r ${./yoinkfiles} push";
-        StandardOutput = "journal";
-        StandardError = "journal";
         Type = "oneshot";
       };
 
-      after = [
-        "local-fs.target" # Ensures all local filesystems are mounted.
-        "home.mount" # Specifically ensures the /home directory is mounted.
-      ];
-
-      wantedBy = ["multi-user.target"];
+      script = ''
+        ${yoink}/bin/yoink -r ${./yoinkfiles} push
+      '';
     };
   };
 }
