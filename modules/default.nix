6aa73529afed73c878a3d60eb418de83ba3ec2f5
Ryan Hedgecock <rhedgeco@gmail.com>
2025-10-06 19:49:30 -0700

    flatten default module

--- modules/default.nix
+++ modules/default.nix
@@ -33,17 +33,16 @@ in {
   # while I prefer FOSS applications, this restriction can be frustrating
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings = {
-    # enables expetimental flakes and nix command features on this system by default
-    # without this, many flake based commands would need `--extra-experimental-features flakes`
-    experimental-features = ["nix-command" "flakes"];
-    # automatically detects files in the store that have identical contents,
-    # and replaces them with hard links to a single copy. This saves disk space.
-    auto-optimise-store = true;
-  };
+  # enables expetimental flakes and nix command features on this system by default
+  # without this, many flake based commands would need `--extra-experimental-features flakes`
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # automatically detects files in the store that have identical contents,
+  # and replaces them with hard links to a single copy. This saves disk space.
+  nix.settings.auto-optimise-store = true;
 
   # do not change unless necessary.
-  # this marks the state version that this config was created with.
+  # this marks the state version that this config was initially created with.
   # as nixos updates this is used to know what config items need to change.
   system.stateVersion = "24.05";
 }
