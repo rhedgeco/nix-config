c02216370135b7b4dc222b904b773c0b72d4713e
Ryan Hedgecock <rhedgeco@gmail.com>
2025-10-22 11:31:51 -0700

    move all cursor enable flags to top

--- users/ryan/cursor.nix
+++ users/ryan/cursor.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   home.pointerCursor = {
     enable = true;
+    gtk.enable = true;
+    x11.enable = true;
     name = "Bibata-Modern-Classic";
     package = pkgs.bibata-cursors;
     size = 22;
-    x11.enable = true;
-    gtk.enable = true;
   };
 }
