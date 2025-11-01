b5d8c2d8561e2c7c084f5dd6b1d3325963d95490
Ryan Hedgecock <rhedgeco@gmail.com>
2025-11-01 11:20:15 -0700

    move bluetooth to jetpack

--- hosts/nixos/jetpack/hardware.nix
+++ hosts/nixos/jetpack/hardware.nix
@@ -6,6 +6,7 @@
   # host specific settings
   nixpkgs.hostPlatform = "x86_64-linux";
   time.timeZone = "America/Los_Angeles";
+  hardware.bluetooth.enable = true;
   services.hardware.bolt.enable = true;
   hardware.enableRedistributableFirmware = true;
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod"];
