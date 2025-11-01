5170c1625e70e95fe1f9433ed71d9350deeba74d
Ryan Hedgecock <rhedgeco@gmail.com>
2025-11-01 15:13:56 -0700

    add service to launch discord at login

--- users/ryan/discord/default.nix
+++ users/ryan/discord/default.nix
@@ -1,22 +1,28 @@
 {
   lib,
   pkgs,
+  iglib,
   config,
   ...
 }: let
   impermanence = config.custom.impermanence;
+  discord = pkgs.discord.override {
+    withOpenASAR = true;
+  };
 in {
-  home.packages = [
-    (pkgs.discord.override {
-      withOpenASAR = true;
-    })
-  ];
+  home.packages = [discord];
 
   # persist the discord directory if impermanence is enabled
   home.persistence = lib.mkIf impermanence.enable {
     "${impermanence.userDir}".directories = [
       ".config/discord"
     ];
+  };
+
+  # create a systemd service that launches discord when the user logs in
+  systemd.user.services.discord-runner = iglib.mkDefaultHomeService {
+    description = "Launches discord on login";
+    script = "${discord}/bin/discord";
   };
 
   # create the discord settings json file
