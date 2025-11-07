5d77a185f82ce0c26e3998ec764ca2f343b2fbac
Ryan Hedgecock <rhedgeco@gmail.com>
2025-11-06 16:32:40 -0800

    add nix community binary cache to config

---
+++ modules/cache.nix
@@ -0,0 +1,14 @@
+{iglib, ...}:
+iglib.module {
+  name = "binary-cache";
+  enabled = true;
+
+  nixos.nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
