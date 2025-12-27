b26dd2d85682be2b001b93c364d55b57046176e4
Ryan Hedgecock <rhedgeco@gmail.com>
2025-12-26 16:25:23 -0800

    rework persist file to be cleaner

--- modules/persist.nix
+++ modules/persist.nix
@@ -3,19 +3,8 @@
   iglib,
   ...
 }: let
-  # a function that merges and replaces null values with defaults from another set
-  mergeNulls = defaults: values:
-    defaults
-    // lib.mapAttrs (name: value:
-      if value == null && defaults ? ${name}
-      then defaults.${name}
-      else value)
-    values;
-
-  # create a bind type for use in options
-  # this defines a set of permission options for a file or directory
-  # a bind type can be a simple string. this will use the default permissions
-  # or the permissions can be defined explicitly using a set of attributes
+  # define a option type that represents a persistent bind
+  # a bind type can be a single string, or a set of attributes
   bindType = lib.types.oneOf [
     lib.types.str
     (lib.types.submodule {
@@ -23,6 +12,11 @@
         target = lib.mkOption {
           description = "The target location to bind to";
           type = lib.types.str;
+        };
+        source = lib.mkOption {
+          description = "The source location to bind to the target";
+          type = lib.types.nullOr lib.types.str;
+          default = null;
         };
         mode = lib.mkOption {
           description = "The permission mode to set for this bind";
@@ -46,9 +40,9 @@ in
   iglib.module {
     name = "persist";
 
-    # set up options to be set by igloo users
-    # these options are not used in the home config,
-    # but is instead used by systems that wish to persist the files
+    # allow home manager users to define persistent bind locations
+    # home-manager cannot itself execute any bind mounts (since bind mounting requires root)
+    # but they can be defined here and any system that can see these options can take advantage
     home.options = {
       files = lib.mkOption {
         description = "Home file targets to bind from persistent storage";
@@ -62,12 +56,12 @@ in
       };
     };
 
-    # nixos persists files using bind mounts
-    # options are extracted from the user config to bind mount home files
+    # on nixos systems, we will also define persistent bind locations
+    # during this process we can also check for home manager users bind paths
     nixos = ctx: {
       options = {
         location = lib.mkOption {
-          description = "The persistent store to bind files from";
+          description = "The path prefix to store all other files in by default";
           type = lib.types.str;
         };
         files = lib.mkOption {
@@ -83,124 +77,119 @@ in
       };
 
       enabled = let
-        # default permissions for system files
-        systemPerms = {
+        # default permissions for system binds
+        bindDefaults = {
+          prefix = "";
           mode = "0755";
           user = "root";
           group = "root";
         };
 
-        # a function to build default permissions for user files
-        mkUserPerms = user: {
-          mode = systemPerms.mode;
-          user = user;
-          group = "users";
+        # a function that generates a normalized bind set
+        normalizeBind = defaults: type: bind: let
+          # a function that forces any string to start with a '/' prefix
+          forceSlash = path:
+            if lib.hasPrefix "/" path
+            then path
+            else "/${path}";
+
+          # a function that resolves a key from the bind
+          # if its null or doesnt exist, it instead uses a default value
+          resolve = key: default:
+            if lib.isAttrs bind && bind ? ${key} && bind.${key} != null
+            then bind.${key}
+            else default;
+
+          # extract the target from the bind
+          # ensure the target always starts with a slash prefix
+          unprefixTarget = forceSlash (resolve "target" bind);
+          target = forceSlash "${defaults.prefix}${unprefixTarget}";
+
+          # extract the source from the bind
+          # if it doesnt exist, calculate the source location
+          source = forceSlash (
+            resolve "source" "${ctx.module.location}${target}"
+          );
+        in {
+          # pass through the is file boolean
+          # use the calculated target and source
+          # define defaults for file permissions
+          inherit type target source;
+          mode = resolve "mode" defaults.mode;
+          user = resolve "user" defaults.user;
+          group = resolve "group" defaults.group;
         };
 
-        # a function that transforms a bind option to its long form
-        # binds can be strings or attribute sets containing permissions
-        # this ensures that the string version is transformed into its attr set version
-        normalizeBind = defaultPerms: value:
-          if lib.isAttrs value
-          then mergeNulls defaultPerms value
-          else defaultPerms // {target = value;};
-
-        # build a function that generates a bash script for binding files
-        # the `create` argument is expected to be a function that takes a single parameter
-        # this is necessary because creating files or directories takes different commands
-        # the rest of the script works regardless of the paths being files or directories
-        bindScript = create: bind: let
-          # map the target to its persistent source location
-          target = lib.removePrefix "/" bind.target;
-          source = "${ctx.module.location}/${target}";
-        in ''
+        # a function to generate a script that sets up a bind mount
+        mkBindScript = bind: ''
           # if the target does not exist, then it needs to be created
-          if [[ ! -e "/${target}" ]]; then
-            ${create target}
+          if [[ ! -e "${bind.target}" ]]; then
+          ${
+            if bind.type == "file"
+            then ''touch "${bind.target}"''
+            else ''mkdir -p ${bind.target}''
+          }
           fi
 
           # set the specified permissions for the target file
           # this always happens so that if the config changes the permissions the file gets updated
-          chown ${bind.user}:${bind.group} "/${target}"
-          chmod ${bind.mode} "/${target}"
+          chown ${bind.user}:${bind.group} "${bind.target}"
+          chmod ${bind.mode} "${bind.target}"
 
           # if the source does not exist, then we can simply copy the target data to the source location
           # this ensures that if the target file existed already, then the existing contents are used
-          if [[ ! -e ${source} ]]; then
+          if [[ ! -e ${bind.source} ]]; then
             # we copy with `-a` here to duplicate all permissions and recursively copy directories as well
-            cp -a "/${target}" "${source}"
+            cp -a "${bind.target}" "${bind.source}"
           fi
 
           # check if the target and source are already bound together
-          if ! findmnt -n -o SOURCE --target "/${target}" | grep -F -q "[${source}]"; then
+          echo "binding '${bind.source}' to '${bind.target}' ..."
+          if ! findmnt -n -o SOURCE --target "${bind.target}" | grep -F -q "[${bind.source}]"; then
             # if the target is not mounted, or the mount is incorrect we should mount it now
             # first, we need to unmount any current binds. we can ignore any failures here
-            umount "/${target}" 2>/dev/null || true
+            umount "${bind.target}" 2>/dev/null || true
 
             # then we need to bind the source to the target
-            echo "binding /${target} ..."
-            mount --bind "${source}" "/${target}"
+            echo "binding '${bind.source}' to '${bind.target}' ..."
+            mount --bind "${bind.source}" "${bind.target}"
           fi
         '';
 
-        # function to build file and directory bind scripts
-        # uses the `bindScript` function, and sets the `create` argument appropriately
-        bindFileScript = bindScript (file: ''touch "/${file}"'');
-        bindDirScript = bindScript (dir: ''mkdir -p "/${dir}"'');
+        # generate normalized binds for system files and dirs
+        fileBinds = map (bind: normalizeBind bindDefaults "file" bind) ctx.module.files;
+        dirBinds = map (bind: normalizeBind bindDefaults "dir" bind) ctx.module.dirs;
 
-        # create a list of normalized bind options for files and dirs
-        # this transforms each bind option into its longform version with permissions
-        sysFileBinds = map (item: normalizeBind systemPerms item) ctx.module.files;
-        sysDirBinds = map (item: normalizeBind systemPerms item) ctx.module.dirs;
-        mkUserFileBinds = user: map (item: normalizeBind (mkUserPerms user) item) (ctx.users.module user).files;
-        mkUserDirBinds = user: map (item: normalizeBind (mkUserPerms user) item) (ctx.users.module user).dirs;
+        # generate normalized binds for user files and dirs
+        userBinds = lib.concatLists (
+          map (
+            user: let
+              userPerms = {
+                prefix = "/home/${user}";
+                mode = bindDefaults.mode;
+                user = "${user}";
+                group = "users";
+              };
+              fileBinds = map (bind: normalizeBind userPerms "file" bind) (ctx.users.module user).files;
+              dirBinds = map (bind: normalizeBind userPerms "dir" bind) (ctx.users.module user).dirs;
+            in
+              fileBinds ++ dirBinds
+          )
+          ctx.users.all
+        );
 
-        # create the scripts for all file and directory binds
-        sysFileBindScripts = lib.listToAttrs (map (bind: {
-            name = "persist '${bind.target}'";
+        # generate all activation scripts from all bind sets
+        bindActivationScripts = lib.listToAttrs (
+          map (bind: {
+            name = "bind '${bind.source}' to '${bind.target}'";
             value = {
               deps = ["users" "groups"];
-              text = bindFileScript bind;
+              text = mkBindScript bind;
             };
-          })
-          sysFileBinds);
-        sysDirBindScripts = lib.listToAttrs (map (bind: {
-            name = "persist '${bind.target}'";
-            value = {
-              deps = ["users" "groups"];
-              text = bindDirScript bind;
-            };
-          })
-          sysDirBinds);
-
-        # create functions that build the binds for user files and directories
-        mkUserFileBindScripts = user:
-          lib.listToAttrs (map (bind: {
-            name = "persist ${user} '${bind.target}'";
-            value = {
-              deps = ["users" "groups"];
-              text = bindFileScript (bind // {target = "/home/${user}/${bind.target}";});
-            };
-          }) (mkUserFileBinds user));
-        mkUserDirBindScripts = user:
-          lib.listToAttrs (map (bind: {
-            name = "persist ${user} '${bind.target}'";
-            value = {
-              deps = ["users" "groups"];
-              text = bindDirScript (bind // {target = "/home/${user}/${bind.target}";});
-            };
-          }) (mkUserDirBinds user));
-
-        # generate and merge all the sys and user bind scripts together
-        fileBindScripts =
-          lib.foldl' (left: right: left // right) sysFileBindScripts
-          (map mkUserFileBindScripts ctx.users.all);
-        dirBindScripts =
-          lib.foldl' (left: right: left // right) sysDirBindScripts
-          (map mkUserDirBindScripts ctx.users.all);
+          }) (fileBinds ++ dirBinds ++ userBinds)
+        );
       in {
-        # merge all file and dir bind scripts into the activation scripts
-        system.activationScripts = fileBindScripts // dirBindScripts;
+        system.activationScripts = bindActivationScripts;
       };
     };
   }
