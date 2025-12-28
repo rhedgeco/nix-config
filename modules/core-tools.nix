8590b40e1e0b620dcb7bf84975e19fcb71eb5f55
Ryan Hedgecock <rhedgeco@gmail.com>
2025-12-28 13:49:30 -0800

    add yoink to core tools

--- modules/core-tools.nix
+++ modules/core-tools.nix
@@ -1,20 +1,25 @@
 {
   pkgs,
   iglib,
+  inputs,
   ...
-}:
-iglib.module {
-  name = "core-tools";
-  enabled = true; # enable these tools by default
+}: let
+  system = pkgs.stdenv.hostPlatform.system;
+  yoink = inputs.yoink.packages.${system}.default;
+in
+  iglib.module {
+    name = "core-tools";
+    enabled = true; # enable these tools by default
 
-  # core cli tools included with any system by default
-  packages = with pkgs; [
-    bat
-    direnv
-    fd
-    gum
-    inotify-tools
-    just
-    python3
-  ];
-}
+    # core cli tools included with any system by default
+    packages = with pkgs; [
+      bat
+      direnv
+      fd
+      gum
+      inotify-tools
+      just
+      python3
+      yoink
+    ];
+  }
