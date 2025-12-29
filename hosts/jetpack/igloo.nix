8b7fafed12512d6f417b6f89ce1f071d33aa6888
Ryan Hedgecock <rhedgeco@gmail.com>
2025-12-29 14:37:34 -0800

    reformat igloo host setup

--- hosts/jetpack/igloo.nix
+++ hosts/jetpack/igloo.nix
@@ -1,37 +1,39 @@
 {...}: {
-  igloo.users.ryan = {
-    enable = true;
-    config = {
-      initialPassword = "ryan";
-      extraGroups = ["wheel"];
-    };
-    home.custom.impermanence = {
+  igloo = {
+    users.ryan = {
       enable = true;
-      userDir = "/persist/home/ryan";
-    };
-  };
-
-  igloo.modules = {
-    grub.enable = true;
-    steam.enable = true;
-    boot.device = "/dev/disk/by-label/BOOT";
-
-    greetd = {
-      enable = true;
-      autoLogin = "ryan";
+      config = {
+        initialPassword = "ryan";
+        extraGroups = ["wheel"];
+      };
+      home.custom.impermanence = {
+        enable = true;
+        userDir = "/persist/home/ryan";
+      };
     };
 
-    persist = {
-      enable = true;
-      location = "/persist";
-      files = [
-        "/etc/machine-id"
-      ];
-      dirs = [
-        "/var/log" # system log files
-        "/var/lib/nixos" # needed for nixos systems
-        "/var/lib/systemd/coredump" # systemd coredump info
-      ];
+    modules = {
+      grub.enable = true;
+      steam.enable = true;
+      boot.device = "/dev/disk/by-label/BOOT";
+
+      greetd = {
+        enable = true;
+        autoLogin = "ryan";
+      };
+
+      persist = {
+        enable = true;
+        location = "/persist";
+        files = [
+          "/etc/machine-id"
+        ];
+        dirs = [
+          "/var/log" # system log files
+          "/var/lib/nixos" # needed for nixos systems
+          "/var/lib/systemd/coredump" # systemd coredump info
+        ];
+      };
     };
   };
 }
