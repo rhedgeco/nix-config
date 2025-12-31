318c8df4f0ce030f192edfbba583f46917d79f83
Ryan Hedgecock <rhedgeco@gmail.com>
2025-12-31 15:22:56 -0800

    dont set up fonts with home manager

--- modules/core-tools.nix
+++ modules/core-tools.nix
@@ -6,35 +6,10 @@
 }: let
   system = pkgs.stdenv.hostPlatform.system;
   yoink = inputs.yoink.packages.${system}.default;
-  fontSetup = {
-    packages = with pkgs; [
-      nerd-fonts.noto
-      nerd-fonts.jetbrains-mono
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-    ];
-
-    fontconfig.defaultFonts = {
-      serif = ["Noto Serif Nerd Font"];
-      sansSerif = ["Noto Sans Nerd Font"];
-      emoji = ["Noto Color Emoji"];
-      monospace = ["JetBrains Mono Nerd Font"];
-    };
-  };
 in
   iglib.module {
     name = "core-tools";
     enabled = true; # enable these tools by default
-
-    nixos.enabled = {
-      # set up fonts with nixos
-      fonts = fontSetup;
-    };
-
-    home.enabled = {
-      # set up fonts with home manager
-      fonts = fontSetup;
-    };
 
     # tools included with any system by default
     packages = with pkgs; [
@@ -50,4 +25,23 @@ in
       python3
       yoink
     ];
+
+    # set up fonts with nixos
+    nixos.enabled = {
+      fonts = {
+        packages = with pkgs; [
+          nerd-fonts.noto
+          nerd-fonts.jetbrains-mono
+          noto-fonts-cjk-sans
+          noto-fonts-color-emoji
+        ];
+
+        fontconfig.defaultFonts = {
+          serif = ["Noto Serif Nerd Font"];
+          sansSerif = ["Noto Sans Nerd Font"];
+          emoji = ["Noto Color Emoji"];
+          monospace = ["JetBrains Mono Nerd Font"];
+        };
+      };
+    };
   }
