283e5fab8d1d5a1ff6743fc3e2d60047d34af64a
Ryan Hedgecock <rhedgeco@gmail.com>
2025-12-31 18:58:23 -0800

    fix core tools layout with fonts

--- modules/core-tools.nix
+++ modules/core-tools.nix
@@ -5,43 +5,51 @@
   ...
 }: let
   system = pkgs.stdenv.hostPlatform.system;
-  yoink = inputs.yoink.packages.${system}.default;
+
+  # core fonts to be included on every system
+  fontPackages = with pkgs; [
+    nerd-fonts.noto
+    nerd-fonts.jetbrains-mono
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+
+  # default font names to apply to all font configs
+  defaultFonts = {
+    serif = ["Noto Serif Nerd Font"];
+    sansSerif = ["Noto Sans Nerd Font"];
+    emoji = ["Noto Color Emoji"];
+    monospace = ["JetBrains Mono Nerd Font"];
+  };
+
+  # core cli tools to be included on every system
+  cliTools = with pkgs; [
+    bat
+    vim
+    nano
+    direnv
+    fd
+    gum
+    inotify-tools
+    just
+    python3
+    (inputs.yoink.packages.${system}.default)
+  ];
 in
   iglib.module {
     name = "core-tools";
     enabled = true; # enable these tools by default
 
-    # tools included with any system by default
-    packages = with pkgs; [
-      # cli tools
-      bat
-      vim
-      nano
-      direnv
-      fd
-      gum
-      inotify-tools
-      just
-      python3
-      yoink
-    ];
-
-    # set up fonts with nixos
+    # apply all packages at the nixos level
     nixos.enabled = {
-      fonts = {
-        packages = with pkgs; [
-          nerd-fonts.noto
-          nerd-fonts.jetbrains-mono
-          noto-fonts-cjk-sans
-          noto-fonts-color-emoji
-        ];
+      environment.systemPackages = cliTools;
+      fonts.packages = fontPackages;
+      fonts.fontconfig.defaultFonts = defaultFonts;
+    };
 
-        fontconfig.defaultFonts = {
-          serif = ["Noto Serif Nerd Font"];
-          sansSerif = ["Noto Sans Nerd Font"];
-          emoji = ["Noto Color Emoji"];
-          monospace = ["JetBrains Mono Nerd Font"];
-        };
-      };
+    # apply all packages at the home manager level
+    home.enabled = {
+      home.packages = cliTools ++ fontPackages;
+      fonts.fontconfig.defaultFonts = defaultFonts;
     };
   }
