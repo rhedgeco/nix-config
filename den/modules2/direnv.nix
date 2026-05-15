3afe00e0471594524d3eb612a9eef48f4f72b379
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-15 11:34:36 -0700

    persist direnv directory

--- den/modules2/direnv.nix
+++ den/modules2/direnv.nix
@@ -1,5 +1,11 @@
 {
   den.aspects.direnv = {
+    persist-home.directories = [
+      # persist the direnv cache
+      # contains allow list, etc...
+      ".local/share/direnv"
+    ];
+
     homeManager = {pkgs, ...}: {
       home.packages = [pkgs.direnv];
       programs.fish.interactiveShellInit = ''
