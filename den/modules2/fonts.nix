a4429df2d5bd6e5ca9ac7d4c9a66bb9cc7e1031c
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-17 09:14:31 -0700

    only worry about nixos system for now

--- den/modules2/fonts.nix
+++ den/modules2/fonts.nix
@@ -1,30 +1,19 @@
-{lib, ...}: let
-  # core fonts to be included on every system
-  fontPackages = pkgs:
-    with pkgs; [
-      nerd-fonts.noto
-      nerd-fonts.jetbrains-mono
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-    ];
-
-  # default font names to apply to all font configs
-  defaultFonts = {
-    serif = lib.mkDefault ["Noto Serif Nerd Font"];
-    sansSerif = lib.mkDefault ["Noto Sans Nerd Font"];
-    emoji = lib.mkDefault ["Noto Color Emoji"];
-    monospace = lib.mkDefault ["JetBrains Mono Nerd Font"];
-  };
-in {
+{lib, ...}: {
   den.default = {
     nixos = {pkgs, ...}: {
-      fonts.packages = fontPackages pkgs;
-      fonts.fontconfig.defaultFonts = defaultFonts;
-    };
+      fonts.packages = with pkgs; [
+        nerd-fonts.noto
+        nerd-fonts.jetbrains-mono
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+      ];
 
-    homeManager = {pkgs, ...}: {
-      home.packages = fontPackages pkgs;
-      fonts.fontconfig.defaultFonts = defaultFonts;
+      fonts.fontconfig.defaultFonts = {
+        serif = lib.mkDefault ["Noto Serif Nerd Font"];
+        sansSerif = lib.mkDefault ["Noto Sans Nerd Font"];
+        emoji = lib.mkDefault ["Noto Color Emoji"];
+        monospace = lib.mkDefault ["JetBrains Mono Nerd Font"];
+      };
     };
   };
 }
--- den/modules2/tools.nix
+++ den/modules2/tools.nix
@@ -1,26 +1,32 @@
-{...}: let
-  mkTools = pkgs:
-    with pkgs; [
-      bat
-      fd
-      ffmpeg
-      gum
-      inotify-tools
-      just
-      nano
-      pciutils
-      python3
-      tree
-      vim
-    ];
-in {
+{
   den.default = {
     nixos = {pkgs, ...}: {
-      environment.systemPackages = mkTools pkgs;
-    };
+      # add bash to the environment shells by default
+      environment.shells = [pkgs.bash];
+      users.defaultUserShell = pkgs.bash;
 
-    homeManager = {pkgs, ...}: {
-      home.packages = mkTools pkgs;
+      # disable the sudo lecture
+      security.sudo.extraConfig = ''
+        Defaults lecture = never
+      '';
+
+      # set the default environment editor to vim
+      environment.variables.EDITOR = "vim";
+
+      # include some packages globally by default
+      environment.systemPackages = with pkgs; [
+        bat
+        fd
+        ffmpeg
+        gum
+        inotify-tools
+        just
+        nano
+        pciutils
+        python3
+        tree
+        vim
+      ];
     };
   };
 }
