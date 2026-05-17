8bc183ce6331dbe10683745a2f76687656106c58
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-17 15:02:29 -0700

    fix copilot typo

--- den/modules2/copilot.nix
+++ den/modules2/copilot.nix
@@ -1,7 +1,7 @@
 {
   den.aspects.copilot = {
     homeManager = {pkgs, ...}: {
-      home.packages = [pkgs.github-github-copilot-cli];
+      home.packages = [pkgs.github-copilot-cli];
       persist.directories = [".config/github-copilot"];
     };
   };
