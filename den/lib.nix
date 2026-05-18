fa533eef82d62c4b6f4b8bc7e116102358f07079
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-18 13:39:13 -0700

    add comment to evalOptions

--- den/lib.nix
+++ den/lib.nix
@@ -1,4 +1,6 @@
 lib: {
+  # convenience function for evaluating options
+  # uses module system to validate configuration settings
   evalOptions = options: config: let
     modules = {modules = [{inherit options config;}];};
   in
