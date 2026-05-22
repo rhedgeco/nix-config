541c38d501e68a067a67ddb4ef1101dceece754f
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-21 17:22:43 -0700

    update layout of evalOptions

--- den/lib.nix
+++ den/lib.nix
@@ -1,8 +1,8 @@
 lib: {
   # convenience function for evaluating options
   # uses module system to validate configuration settings
-  evalOptions = options: config: let
-    modules = {modules = [{inherit options config;}];};
-  in
-    (lib.evalModules modules).config;
+  evalOptions = options: config:
+    (lib.evalModules {
+      modules = [{inherit options config;}];
+    }).config;
 }
