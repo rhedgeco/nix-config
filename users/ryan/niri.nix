bf9b25fd962cc8b4b66df544b76771962825f209
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-23 22:59:56 -0700

    switch niri to use animated abitoad wallpaper

--- users/ryan/niri.nix
+++ users/ryan/niri.nix
@@ -1,8 +1,41 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  wallpaperAlign = -0.0;
+  wallpaperSrc = ./wallpapers/LazyRiver.mp4;
+  mpvFlags = [
+    # Hardware Acceleration
+    "--hwdec=auto-safe" # automatically use the best hardware driver
+    "--vd-lavc-dr=yes" # enable direct rendering
+    "--gpu-context=wayland" # use wayland gpu context
+
+    # Streaming, Synchronization
+    "--loop-file=yes" # set to infinite loop from local file
+    "--no-audio" # dont use audio engine, or initialize sound cards
+
+    # Positioning, Framing, Scaling
+    "--panscan=1.0" # center and fill the video stream
+    "--video-align-y=${toString wallpaperAlign}" # shift vertical alignment
+    "--dscale=bilinear" # use bilinear scaling when downscaling
+    "--cscale=bilinear" # use the same scaling for chroma scaling
+    "--sws-scaler=fast-bilinear" # fallback to fast software scaler
+    "--correct-downscaling=no" # tell the downscaler that it doesnt have to be "correct" and bilinear is fine
+  ];
+  wallpaperRunner = pkgs.writeShellScript "anim-wallpaper" ''
+    # Run mpvpaper using an infinite anonymous streaming pipe
+    # use ffmpeg as the stream pipe to play an infinitely looping mp4 video
+    exec ${pkgs.mpvpaper}/bin/mpvpaper -o "${lib.concatStringsSep " " mpvFlags}" ALL "${wallpaperSrc}"
+  '';
+in {
   igloo.modules.niri = {
     enable = true;
     spawn = [
       "discord"
+
+      # run the animated wallpaper
+      "${wallpaperRunner}"
 
       # meme activate linux overlay
       "${pkgs.activate-linux}/bin/activate-linux"
