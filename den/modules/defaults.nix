a9588e575e02b21a7f25f51b193bc54def9e52e1
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-26 23:14:10 -0700

    re-work default.nix file to be cleaner

--- den/modules/defaults.nix
+++ den/modules/defaults.nix
@@ -3,9 +3,7 @@
   den,
   inputs,
   ...
-}: let
-  apply = paths: config: lib.genAttrs paths (_: config);
-in {
+}: {
   # import flakeModule to generate top level flake structure
   imports = [inputs.den.flakeModule];
 
@@ -17,32 +15,27 @@ in {
   den.schema.user.includes = [(den.batteries.import-tree.provides.user ./users)];
 
   # apply default settings across all classes
-  # we use a merge strategy to combine different sets of settings together
-  den.default = lib.mkMerge [
-    {
-      # Sets the system hostname from den.hosts.<name>.hostName. Works on NixOS and Darwin.
-      includes = [den.batteries.hostname den.batteries.flake-scope];
+  den.default = {
+    includes = [den.batteries.hostname den.batteries.flake-scope];
 
-      # initial state versions for this configuration
-      # these should not be changed unless you know what you are doing
-      nixos.system.stateVersion = "24.05";
-      homeManager.home.stateVersion = "24.05";
-    }
+    # allow unfree packages globally
+    nixos.nixpkgs.config.allowUnfree = true;
+    homeManager.nixpkgs.config.allowUnfree = true;
 
-    # apply duplicate settings to both nixos and home manager
-    (apply ["nixos" "homeManager"] {
-      # always allow unfree packages by default
-      # i dont care enough to deal with that every time
-      nixpkgs.config.allowUnfree = true;
+    # initial state versions for this configuration
+    # these should not be changed unless you know what you are doing
+    nixos.system.stateVersion = "24.05";
+    homeManager.home.stateVersion = "24.05";
+
+    # nix daemon settings (nixos only)
+    nixos = {
       # sets the system 'nixpkgs' path to match the one used in this flake
-      # this means when the <nixpkgs> syntax is used it will match system packages
+      # this means <nixpkgs> syntax will match system packages
       nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-      # automatically detects files in the store that have identical contents,
-      # and replaces them with hard links to a single copy. This saves disk space.
+      # replaces identical files in the store with hard links to save disk space
       nix.settings.auto-optimise-store = true;
-      # enables expetimental flakes and nix command features on this system by default
-      # without this, many flake based commands would need `--extra-experimental-features flakes`
+      # enables flakes and the nix command without --extra-experimental-features
       nix.settings.experimental-features = ["nix-command" "flakes"];
-    })
-  ];
+    };
+  };
 }
