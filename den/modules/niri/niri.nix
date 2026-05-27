afa51c309067992d722c41ac678e0f1ff093bab9
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-27 15:24:54 -0700

    add sessions and make niri contribute to the packages

--- den/modules/niri/niri.nix
+++ den/modules/niri/niri.nix
@@ -1,9 +1,5 @@
 {...}: {
   den.aspects.niri = {
-    nixos = {pkgs, ...}: {
-      environment.systemPackages = [pkgs.niri];
-    };
-
     provides.to-users.homeManager = {pkgs, ...}: {
       sessions.niri = {
         packages = [pkgs.niri];
