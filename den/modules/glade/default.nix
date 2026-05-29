e4722b5cee7df523365262a647ceb254338cf71f
Ryan Hedgecock <rhedgeco@gmail.com>
2026-05-28 17:53:24 -0700

    flush out glade niri integration

--- den/modules/glade/default.nix
+++ den/modules/glade/default.nix
@@ -1,13 +1,33 @@
 {
   den.aspects.glade = {
     nixos = {pkgs, ...}: let
-      niri-config = ./_assets/niri-config.kdl;
+      # build the default niri configuration for glade
+      niri-static = ./_assets/niri-static.kdl;
+      niri-config = pkgs.writeText "niri-config.kdl" ''
+        // static glade configuration
+        include "${niri-static}"
+
+        // include some custom nix defined binds
+        binds {
+          // spawn alacritty as the terminal emulator
+          Mod+T { spawn "${pkgs.alacritty}/bin/alacritty"; }
+        }
+
+        // optional user overrides in home directory
+        // included last so that anything can be overriden
+        include optional=true "~/.config/glade/niri.kdl"
+      '';
+
+      # write a session script to configure and launch the DE
+      # doing it this way ensures all config is glade specific
+      # and wont get mixed up with config from another DE
       glade-session = pkgs.writeShellScript "glade-session" ''
         export PATH="$HOME/.config/glade/packages/bin:$PATH"
         export NIRI_CONFIG=${niri-config}
         exec niri-session
       '';
     in {
+      # add custom glade wayland desktop entry
       services.displayManager.sessionPackages = [
         (pkgs.writeTextDir "share/wayland-sessions/glade.desktop" ''
           [Desktop Entry]
@@ -33,10 +53,13 @@
       };
 
       config = {
+        # include some packages with glade by default
         glade.packages = with pkgs; [
           niri
         ];
 
+        # write all default and user specified packages to the glade directory
+        # these can then be added to the PATH environment before the session launches
         home.file.".config/glade/packages".source = pkgs.buildEnv {
           name = "glade-packages";
           paths = config.glade.packages;
