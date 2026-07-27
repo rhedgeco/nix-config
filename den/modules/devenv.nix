365cfd2128c6b25b56c41a4272fcb18a5b277b8d
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-26 23:12:13 -0700

    add packages to home with direnv and devenv

--- den/modules/devenv.nix
+++ den/modules/devenv.nix
@@ -3,7 +3,7 @@
     # persist the devenv cache
     # contains allow list, etc...
     persist.dirs = [".local/share/devenv"];
-
+    home.packages = [pkgs.devenv];
     programs.fish.interactiveShellInit = ''
       # load devenv shell hook
       ${pkgs.devenv}/bin/devenv hook fish | source
--- den/modules/direnv.nix
+++ den/modules/direnv.nix
@@ -3,7 +3,7 @@
     # persist the direnv cache
     # contains allow list, etc...
     persist.dirs = [".local/share/direnv"];
-
+    home.packages = [pkgs.direnv];
     programs.fish.interactiveShellInit = ''
       # load direnv shell hook
       ${pkgs.direnv}/bin/direnv hook fish | source
