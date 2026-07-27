1dec6a785870dd88d3c52ebf90ef4dd0ae306e5b
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-27 13:43:10 -0700

    use raw instead of anything to make sure the raw data gets forwarded

--- den/modules/include.nix
+++ den/modules/include.nix
@@ -7,7 +7,7 @@
   den.schema.host = {
     includes = [den.aspects.host-schema-include];
     options.includes = lib.mkOption {
-      type = with lib.types; listOf anything;
+      type = with lib.types; listOf raw;
       default = [];
     };
   };
@@ -16,7 +16,7 @@
   den.schema.user = {
     includes = [den.aspects.user-schema-include];
     options.includes = lib.mkOption {
-      type = with lib.types; listOf anything;
+      type = with lib.types; listOf raw;
       default = [];
     };
   };
@@ -25,7 +25,7 @@
   den.schema.home = {
     includes = [den.aspects.home-schema-include];
     options.includes = lib.mkOption {
-      type = with lib.types; listOf anything;
+      type = with lib.types; listOf raw;
       default = [];
     };
   };
--- den/modules/persist.nix
+++ den/modules/persist.nix
@@ -69,12 +69,12 @@
         options = {
           dirs = lib.mkOption {
             description = "directories to persist";
-            type = with lib.types; listOf anything;
+            type = with lib.types; listOf raw;
             default = [];
           };
           files = lib.mkOption {
             description = "directories to persist";
-            type = with lib.types; listOf anything;
+            type = with lib.types; listOf raw;
             default = [];
           };
         };
