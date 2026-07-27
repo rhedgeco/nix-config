5b9cc46fe2f559c2a86ad94a6e83a34ec9986fbd
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-27 14:18:31 -0700

    ensure flatpak has a portals enabled

--- den/modules/flatpak.nix
+++ den/modules/flatpak.nix
@@ -1,6 +1,14 @@
 {
-  den.aspects.flatpak.nixos = {
+  den.aspects.flatpak.nixos = {pkgs, ...}: {
     services.flatpak.enable = true;
     persist.dirs = ["/var/lib/flatpak"];
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config.common.default = ["gtk"];
+    };
   };
 }
