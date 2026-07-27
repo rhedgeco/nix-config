4ff91b47815d0bbb019ee36df899726da1160e71
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-26 22:34:25 -0700

    migrate embedded config

--- den/modules/embedded.nix
+++ den/modules/embedded.nix
@@ -3,9 +3,16 @@
     # any user with this aspect should have dialout access
     user.extraGroups = ["dialout"];
 
+    # udev packages have to be added at the nixos level
     nixos = {pkgs, ...}: {
-      # udev packages have to be added to give them special access
       services.udev.packages = with pkgs; [
+        saleae-logic-2
+        stlink-gui
+      ];
+    };
+
+    homeManager = {pkgs, ...}: {
+      home.packages = with pkgs; [
         saleae-logic-2
         stlink-gui
       ];
