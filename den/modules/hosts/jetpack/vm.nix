8e837756f29827e47739db53e02bc8e4c316ede8
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-27 23:20:32 -0700

    set up vm to be a little more performant

--- den/modules/hosts/jetpack/vm.nix
+++ den/modules/hosts/jetpack/vm.nix
@@ -1,25 +1,34 @@
 {
-  den.aspects.jetpack.nixos = {
-    # set up the vm filesystem with everything it needs
-    virtualisation.vmVariant.virtualisation = {
-      diskImage = null; # tmpfs
+  den.aspects.jetpack.nixos.virtualisation.vmVariant = {
+    # enable hardware accelerated graphics
+    hardware.graphics.enable = true;
+
+    virtualisation = {
+      # use a tmpfs disk image
+      diskImage = null;
+
+      # give enough memory so some apps dont have to swap
+      memorySize = 8192;
+
+      # set up persistent disk so stores dont fail
+      # this is still on the tmpfs and will not actually persist
       fileSystems."/persist" = {
         device = "tmpfs";
         fsType = "tmpfs";
         options = ["mode=755"];
         neededForBoot = true;
       };
+
+      qemu.options = [
+        "-cpu host"
+        "-enable-kvm"
+
+        # niri (smithay) needs a GL/GLES render node
+        # proivide the VM a virgl-capable virtio GPU
+        "-vga none"
+        "-device virtio-vga-gl"
+        "-display gtk,gl=on,grab-on-hover=on"
+      ];
     };
-
-    # niri (smithay) needs a GL/GLES render node
-    # proivide the VM a virgl-capable virtio GPU
-    virtualisation.vmVariant.virtualisation.qemu.options = [
-      "-vga none"
-      "-device virtio-vga-gl"
-      "-display gtk,gl=on,grab-on-hover=on"
-    ];
-
-    # enable graphics so EGL/GBM can find the virgl driver
-    virtualisation.vmVariant.hardware.graphics.enable = true;
   };
 }
