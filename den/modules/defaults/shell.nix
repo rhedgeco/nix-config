b61357ca12d9b862e430c14d2fa18ae196b65a9f
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 10:00:17 -0700

    migrate shell config

--- den/modules/defaults/shell.nix
+++ den/modules/defaults/shell.nix
@@ -1,5 +1,5 @@
-{pkgs, ...}: {
-  config = {
+{
+  den.default.nixos = {pkgs, ...}: {
     # add bash to the environment shells by default
     environment.shells = [pkgs.bash];
     users.defaultUserShell = pkgs.bash;
@@ -8,8 +8,5 @@
     security.sudo.extraConfig = ''
       Defaults lecture = never
     '';
-
-    # set the default environment editor to vim
-    environment.variables.EDITOR = "vim";
   };
 }
