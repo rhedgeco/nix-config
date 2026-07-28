58638513cbbcbc138a1dcc6e7556821e8c3ced15
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 13:49:30 -0700

    remove igloo

--- inputs/igloo/modules/nixos.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}: {
-  imports = [./global.nix];
-
-  options.igloo.home = lib.mkOption {
-    description = "Configuration to apply to every igloo user";
-    type = lib.types.attrs;
-    default = {};
-  };
-}
