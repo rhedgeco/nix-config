8c98f21d2e33066805616277bf3f7a49ea62f1ff
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-27 22:50:30 -0700

    dont to backups with create

--- den/modules/aspects/create.nix
+++ den/modules/aspects/create.nix
@@ -22,10 +22,10 @@
             TARGET="${config.home.homeDirectory}/${target}"
             SOURCE="${source}"
 
-            # backup the target file if it already exists
+            # remove the target file if it already exists
             if [ -e "$TARGET" ] || [ -L "$TARGET" ]; then
-              echo "Found existing file at $TARGET. Making backup..."
-              mv "$TARGET" "$TARGET.$(date +%Y%m%d_%H%M%S).bak"
+              echo "Found existing file at $TARGET. Removing..."
+              rm -rf "$TARGET"
             fi
 
             # create the directory if it doesnt exist
