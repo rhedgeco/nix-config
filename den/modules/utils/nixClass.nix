ea9b7ad1cf932d14fe50270eaf5a68706e8d76a1
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 09:31:55 -0700

    add nix class and use it for cache

---
+++ den/modules/utils/nixClass.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  den,
+  ...
+}: let
+  # forwards nix content into both nixos and home manager
+  nixClass = {aspect-chain, ...}:
+    den.batteries.forward {
+      each = ["nixos" "homeManager"];
+      fromClass = _: "nix";
+      intoClass = lib.id;
+      intoPath = _: ["nix"];
+      fromAspect = _: lib.head aspect-chain;
+      adaptArgs = lib.id;
+    };
+in {
+  # enable class for all users:
+  den.schema.user.includes = [nixClass];
+}
