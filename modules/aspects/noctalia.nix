60ba0a46e783edd66151462db833b4f1ea773a00
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 14:49:04 -0700

    update noctalia settings

--- modules/aspects/noctalia.nix
+++ modules/aspects/noctalia.nix
@@ -3,5 +3,16 @@
     home.packages = [
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
+
+    persist.files = [
+      # contains calendar and other state that should persist
+      ".local/state/noctalia/state.toml"
+    ];
+
+    # set sensible defaults for noctalia here
+    create.".config/noctalia/den.toml" = ''
+      [general]
+      show_welcome = false
+    '';
   };
 }
