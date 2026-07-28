79013f50b5ea15567679cfd3c19cac56b425ad4b
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 09:18:45 -0700

    migrate vicinae settings

---
+++ den/modules/hosts/jetpack/ryan/vicinae.nix
@@ -0,0 +1,13 @@
+{
+  den.aspects.jetpack.provides.ryan.homeManager = {
+    vicinae = {
+      favorites = [
+        "applications:steam"
+        "applications:discord"
+        "applications:dev.zed.Zed.desktop"
+        "applications:org.gnome.Nautilus"
+        "applications:firefox"
+      ];
+    };
+  };
+}
