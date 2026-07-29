90661ed019d45aa6c36c48cdc825aafaf989fbae
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 18:07:10 -0700

    remove garnix cache

--- modules/defaults/cache.nix
+++ modules/defaults/cache.nix
@@ -2,13 +2,11 @@
   den.default.nixos.nix.settings = {
     substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.garnix.io"
       "https://zed.cachix.org"
       "https://noctalia.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
