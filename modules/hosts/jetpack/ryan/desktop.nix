9a3baf058b805e408a3fac56f86c8abde416297c
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-28 17:11:28 -0700

    update theme handling

--- modules/hosts/jetpack/ryan/desktop.nix
+++ modules/hosts/jetpack/ryan/desktop.nix
@@ -41,6 +41,7 @@
 
     # write the noctalia settings configuration
     create.".config/noctalia/settings.toml" = ./_assets/noctalia/settings.toml;
+    create.".config/noctalia/palettes/Cream.json" = ./_assets/noctalia/Cream.json;
 
     # have niri spawn a video wallpaper at startup
     niri.include."mpvpaper.kdl" = let
