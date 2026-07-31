9397e569591bfbec4863f3ab1bb3b08812d39b74
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-31 16:41:00 -0700

    disable noctalia startup banner

--- modules/aspects/noctalia.nix
+++ modules/aspects/noctalia.nix
@@ -4,6 +4,10 @@
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
+    # create the empty setup complete file
+    # this prevents the welcome banner from appearing
+    create.".local/state/noctalia/.setup-complete" = "";
+
     persist.dirs = [
       # only persist the calendar part of the cache
       # so calendar events persist without network
