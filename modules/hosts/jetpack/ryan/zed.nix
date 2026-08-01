3253d33e838c87c8ea80d5454612765068e7fa56
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-31 21:45:06 -0700

    switch zed to custom settings control

--- modules/hosts/jetpack/ryan/zed.nix
+++ modules/hosts/jetpack/ryan/zed.nix
@@ -8,74 +8,37 @@
     includes = [den.aspects.ai-tools];
 
     homeManager = {pkgs, ...}: {
+      home.packages = with pkgs; [
+        # include the main editor
+        zed-editor
+
+        # include supporting packages for editing nix
+        nixd
+        nil
+      ];
+
       # persist zed local share for now
       # TODO: add declarative local share content
       persist.dirs = [
         ".local/share/zed"
       ];
 
-      # enable and configure the zed editor
-      programs.zed-editor = {
-        enable = true;
-        extensions = [
-          "nix"
-          "toml"
-          "rust"
-          "xml"
-          "html"
-          "catppuccin-icons"
-          "crates-lsp"
-          "git-firefly"
-          "kdl"
-        ];
-        extraPackages = with pkgs; [
-          nil
-          nixd
-          alejandra
-        ];
-        themes = {
-          "Dark Modern" = ./_assets/zed/DarkModern.json;
-        };
-        userSettings = {
-          ui_font_size = 16;
-          buffer_font_size = 14;
-          minimap.show = "always";
-          format_on_save = "on";
-          title_bar.show_menus = true;
-          outline_panel.dock = "left";
-          project_panel.dock = "left";
-          icon_theme = "Catppuccin Mocha";
-          file_types = {
-            "TOML" = ["**/*.yoink"];
-          };
-          theme = {
-            mode = "system";
-            dark = "Dark Modern";
-            light = "One Light";
-          };
-          indent_guides.coloring = "indent_aware";
-          git_panel = {
-            dock = "left";
-            tree_view = true;
-          };
-          agent = {
-            dock = "right";
-            default_model = {
-              provider = "copilot_chat";
-              model = "claude-opus-4.8";
-            };
-            thread_summary_model = {
-              provider = "copilot_chat";
-              model = "claude-sonnet-4.8";
-            };
-          };
+      # apply the zed theme
+      create.".config/zed/themes/DarkModern.json" = ./_assets/zed/DarkModern.json;
+
+      # apply the zed settings
+      create.".config/zed/settings.json" = builtins.toJSON (
+        lib.recursiveUpdate
+        # load the existing json file
+        (builtins.fromJSON (builtins.readFile ./_assets/zed/settings.json))
+        # then merge some custom nix values into it
+        {
           lsp = {
             package-version-server.binary.path = lib.getExe pkgs.package-version-server;
             crates-lsp.binary.path = lib.getExe pkgs.crates-lsp;
-            nil.initialization_options.formatting.command = ["alejandra"];
           };
-        };
-      };
+        }
+      );
     };
   };
 }
