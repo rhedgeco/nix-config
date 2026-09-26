f7b4f72aa9e5b257752f1f96b470bf2143346543
Ryan Hedgecock <rhedgeco@gmail.com>
2026-09-26 10:35:56 -0700

    make windows togo drive only show up when plugged in

--- modules/aspects/windows-togo.nix
+++ modules/aspects/windows-togo.nix
@@ -4,23 +4,18 @@
       # set the hardware clock to local time to play nicely with windows
       time.hardwareClockInLocalTime = true;
 
-      # add a grub entry for the windows togo drive
-      boot.loader.grub.extraEntries = ''
-        menuentry "${name}" --class windows11 --class windows --class os --id windows-togo-${uuid} {
-            insmod part_gpt
-            insmod part_msdos
-            insmod fat
-            insmod search_fs_uuid
-            insmod chain
-            search --no-floppy --fs-uuid --set=root ${uuid}
-            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-        }
-      '';
-
-      # if the windows togo drive is plugged in, boot it by default
+      # only define the grub entry when the windows togo drive is plugged in
       boot.loader.grub.extraConfig = ''
         if search --no-floppy --fs-uuid --set=root ${uuid}; then
-            set default="windows-togo-${uuid}"
+            menuentry "${name}" --class windows11 --class windows --class os --id windows-togo-${uuid} {
+                insmod part_gpt
+                insmod part_msdos
+                insmod fat
+                insmod search_fs_uuid
+                insmod chain
+                search --no-floppy --fs-uuid --set=root ${uuid}
+                chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+            }
         fi
       '';
     };
